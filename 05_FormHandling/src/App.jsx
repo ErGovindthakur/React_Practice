dbd05e005aa10c842f1d030f927e057bab61d369
ErGovindthakur <ergovindthakur@gmail.com>
2025-10-19 01:05:00 +0530

    explore form handling in react

---
+++ 05_FormHandling/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MultiFieldForm from './MultiFieldForm'
+
+const App = () => {
+  return (
+    <div className='w-full h-screen bg-zinc-900 text-white flex justify-center items-center'>
+      <MultiFieldForm />
+    </div>
+  )
+}
+
+export default App
