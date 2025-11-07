c408dae17677e29f3b17c12437e8c56391df267e
ErGovindthakur <ergovindthakur@gmail.com>
2025-11-07 22:17:16 +0530

    write some specific routes

--- 07_CrudWithAuth/backend/Product/ProductRoutes.js
+++ 07_CrudWithAuth/backend/Product/ProductRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createProduct, deleteProduct, getProducts, updateProduct } from "./ProductController.js";
+import { createProduct, deleteProduct, getProducts, updateProduct,getSingleProduct } from "./ProductController.js";
 
 export const router = express.Router();
 
 router.get("/getProducts",getProducts);
-router.put("/updateProduct",updateProduct);
+router.get("/getSingleProduct/:id",getSingleProduct);
+router.put("/updateProduct/:id",updateProduct);
 router.post("/createProduct",createProduct);
-router.delete("/deleteProduct",deleteProduct)
+router.delete("/deleteProduct/:id",deleteProduct)
